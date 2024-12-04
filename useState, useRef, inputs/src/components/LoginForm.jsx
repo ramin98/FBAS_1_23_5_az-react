a8e0fdfddc8bcebddb7a9f8b8eae22efd4bf7b75
ramin98 <59912261+ramin98@users.noreply.github.com>
2024-12-04 18:03:40 +0400

    Update LoginForm.jsx

--- useState, useRef, inputs/src/components/LoginForm.jsx
+++ useState, useRef, inputs/src/components/LoginForm.jsx
@@ -1,52 +1,133 @@
-import { useState } from "react";
+// import { useState, useRef } from "react";
+
+// function LoginForm() {
+//   let [login, setLogin] = useState("");
+//   let [password, setPassword] = useState("");
+
+//   let loginCheck = useRef(false);
+//   let passwordCheck = useRef(false);
+
+//   function handleLogin(ev) {
+//     setLogin(ev.target.value);
+//     console.log(login);
+//   }
+
+//   function handlePassword(ev) {
+//     setPassword(ev.target.value);
+//     console.log(password);
+//   }
+
+//   let loginSymbolLength = login.length >= 8 
+//   let loginSymbolCase =  login.split('').every((item) => item.toLowerCase() == item)
+
+//   let loginSymbolIs = login.split('').some((item) => Number.isSafeInteger(parseInt(item)))
+
+//   let passwordSymbolLength = password.length >= 8 
+//   let passwordSymbolCase
+//   if(password !== ''){
+//     passwordSymbolCase = password[0] === password[0].toUpperCase()
+//   }
+  
+//   let passwordSymbolIs = password.split('').some((item) => Number.isSafeInteger(parseInt(item)))
+//   let passwordSymbolHas = password.includes('.')
+
+//   loginCheck.current = loginSymbolLength && loginSymbolCase && !loginSymbolIs
+//   passwordCheck.current = passwordSymbolLength && passwordSymbolCase && passwordSymbolIs && passwordSymbolHas
+
+//   let active = passwordCheck.current && passwordCheck.current
+
+//   return (
+//     <section>
+//       <form>
+//         LOGIN
+//         <label>
+//           <input
+//             onInput={handleLogin}
+//             type="text"
+//             placeholder="login"
+//             name="login"
+//           />
+//           <p className={loginCheck.current ? 'right' : 'wrong'}>{loginCheck.current ? 'RIGHT' : 'WRONG'}</p>
+//         </label>
+//         <label>
+//           PASSWORD
+//           <input
+//             type="text"
+//             onInput={handlePassword}
+//             placeholder="password"
+//             name="password"
+//           />
+//           <p className={passwordCheck.current ? 'right' : 'wrong'}>{passwordCheck.current ? 'RIGHT' : 'WRONG'}</p>
+//         </label>
+//         <button disabled={!active}>LOGIN</button>
+//       </form>
+//     </section>
+//   );
+// }
+
+// export default LoginForm;
+
+
+import { useState, useRef } from "react";
 
 function LoginForm() {
   let [login, setLogin] = useState("");
   let [password, setPassword] = useState("");
-//   let [active, setActive] = useState(true);
 
+  let loginCheck = useRef(false);
+  let passwordCheck = useRef(false);
 
   function handleLogin(ev) {
     setLogin(ev.target.value);
     console.log(login);
-    // inputsCheck()
   }
 
   function handlePassword(ev) {
     setPassword(ev.target.value);
     console.log(password);
-    // inputsCheck()
   }
 
-//   function inputsCheck() {
-//     if(login.length > 8 && password.length > 8){
-//         setActive(false)
-//     }else{
-//         setActive(true)
-//     }
-//   }
+  const loginRegex = /^[a-z]{8,}$/;
 
-  let active = login.length >= 8 && password.length >= 8 ? false : true
+  const passwordRegex = /^(?=.*[A-Z])(?=.*\d)(?=.*\.).{8,}$/; 
+
+  loginCheck.current = loginRegex.test(login);
+
+  passwordCheck.current = passwordRegex.test(password);
+
+  let active = loginCheck.current && passwordCheck.current;
 
   return (
     <section>
       <form>
-        <input
-          onInput={handleLogin}
-          type="text"
-          placeholder="login"
-          name="login"
-        />
-        <input
-          type="password"
-          onInput={handlePassword}
-          placeholder="password"
-          name="password"
-        />
-        <button disabled={active}>LOGIN</button>
+        LOGIN
+        <label>
+          <input
+            onInput={handleLogin}
+            type="text"
+            placeholder="login"
+            name="login"
+          />
+          <p className={loginCheck.current ? "right" : "wrong"}>
+            {loginCheck.current ? "RIGHT" : "WRONG"}
+          </p>
+        </label>
+        <label>
+          PASSWORD
+          <input
+            type="text"
+            onInput={handlePassword}
+            placeholder="password"
+            name="password"
+          />
+          <p className={passwordCheck.current ? "right" : "wrong"}>
+            {passwordCheck.current ? "RIGHT" : "WRONG"}
+          </p>
+        </label>
+        <button disabled={!active}>LOGIN</button>
       </form>
     </section>
   );
 }
 
-export default LoginForm;
+export default LoginForm
