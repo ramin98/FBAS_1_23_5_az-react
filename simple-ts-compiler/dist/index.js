96404467af8497ac70f9b2b525ea9c1f6e8dccfa
ramin98 <59912261+ramin98@users.noreply.github.com>
2025-01-06 17:52:33 +0400

    Add files via upload

---
+++ simple-ts-compiler/dist/index.js
@@ -0,0 +1,14 @@
+function greet(name) {
+    return `Hello, ${name}!`;
+}
+console.log(greet("World"));
+function greet2(name) {
+    return `Hello, ${name}!`;
+}
+console.log(greet("World"));
+function greet3(name) {
+    return `Hello, ${name}!`;
+}
+console.log(greet("World"));
+let a = 78;
+let b = 67;
